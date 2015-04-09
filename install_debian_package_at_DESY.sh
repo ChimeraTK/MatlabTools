1ce824e8fd709ab942e4edb19de461ab43a69508
killenb <killenb@0a4bbe00-8642-4806-bb6d-a080d43f46e8>
2015-04-09 08:13:00 +0000

    fixed debian install script for desy
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u_applications/matlab_tools/trunk@18 0a4bbe00-8642-4806-bb6d-a080d43f46e8

--- install_debian_package_at_DESY.sh
+++ install_debian_package_at_DESY.sh
@@ -23,10 +23,11 @@ cd debian_package
 ssh doocspkgs sudo -H reprepro --waitforlock 2 -Vb \
     /export/reprepro/intern/doocs remove ${DEBIAN_CODENAME} mtca4u-matlab-tools
 
-# Step 2: Set the priviledge mask for new files to rw-rw-r--
+# Step 2: Set the priviledges and the priviledge mask for new files to rw-rw-r--
+chmod 664 ${PACKAGE_FILES_WILDCARDS}
 umask 002
 # -- and copy the files to the nfs archive, using sg to copy with the flash group as target group
-sg flash -c "cp @PACKAGE_FILES_WILDCARDS@ /home/debian/${DEBIAN_CODENAME}/stable"
+sg flash -c "cp ${PACKAGE_FILES_WILDCARDS} /home/debian/${DEBIAN_CODENAME}/stable"
 
 # Step 3: Install to the repository (only desy intern)
 for FILE in *.deb; do
