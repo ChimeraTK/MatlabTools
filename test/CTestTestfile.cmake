d549970787c78347b8c3300649426365fbad0dbe
nshehzad <nshehzad@0a4bbe00-8642-4806-bb6d-a080d43f46e8>
2016-01-26 13:41:59 +0000

    added: Example to the tests for code coverage.
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u_applications/matlab_tools/trunk@83 0a4bbe00-8642-4806-bb6d-a080d43f46e8

--- test/CTestTestfile.cmake
+++ test/CTestTestfile.cmake
@@ -5,6 +5,7 @@ ADD_TEST(local_open_close "./mleval" "run test_local_open_close.m" "-s")
 ADD_TEST(local_version "./mleval" "run init_local; run test_version.m" "-s")
 ADD_TEST(local_read "./mleval" "run init_local; run test_read.m" "-s")
 ADD_TEST(local_write "./mleval" "run init_local; run test_write.m" "-s")
+ADD_TEST(example "./mleval" "run init_local; run ../example/example.m" "-s")
 
 ADD_TEST(remote_init "./mleval" "run test_remote_init.m" "-s")
 ADD_TEST(remote_version "./mleval" "run init_remote; run test_version.m" "-s")
