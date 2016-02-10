9334c6cf5452c379c340196999ae640eb56200ed
nshehzad <nshehzad@0a4bbe00-8642-4806-bb6d-a080d43f46e8>
2016-02-10 13:27:08 +0000

    added: set_damp at the top to test.
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u_applications/matlab_tools/trunk@91 0a4bbe00-8642-4806-bb6d-a080d43f46e8

--- test/test_read.m
+++ test/test_read.m
@@ -1,6 +1,7 @@
 %%
 %
-
+mtca4u_mex('set_dmap','dummies.dmap');
+m = mtca4u('DUMMY1');
 % Syntax of read:  module, register, [offset], [elements]
 
 %% Check Basic read functions with know return values
