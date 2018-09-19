78d5503498fb81eb2e7cf88be991ac6f036d1298
Martin Killenberg <martin.killenberg@desy.de>
2018-09-19 13:05:21 +0200

    adapted required DeviceAccess command line tools version to the latest version

--- matlab/mtca4u_remote.m
+++ matlab/mtca4u_remote.m
@@ -35,9 +35,9 @@ classdef mtca4u_remote < mtca4u_interface
 		board = [];
         channel = 0;
         c;
-        required_tools_version = '00.12';
+        required_tools_version = '00.13';
         %remote_executable = strcat('mtca4u-', self.required_tools_version);
-        remote_executable = 'mtca4u-00.12';
+        remote_executable = 'mtca4u-00.13';
     end
     
     methods (Access = 'private')
