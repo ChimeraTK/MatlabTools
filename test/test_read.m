297f165cb8975c2a7a53d258dc4353203e8e478c
vargheseg <vargheseg@users.noreply.github.com>
2019-05-20 13:02:59 +0200

    extended tests for read_raw

--- test/test_read.m
+++ test/test_read.m
@@ -8,7 +8,11 @@ m = mtca4u('DUMMY1');
 
 assert(m.read('', 'WORD_FIRMWARE') == 0, 'Wrong firmware id returned.');
 assert(m.read('', 'WORD_COMPILATION') == 9, 'Wrong compilation returned.');
+assert(m.read_raw('', 'WORD_COMPILATION') == 9, 'Wrong compilation returned.');
 %assert(m.read('', 'WORD_CLK_DUMMY') == 1145916761, 'Wrong clk_dummy returned.');
+
+m.write('', 'WORD_USER', 10.25)
+assert(m.read_raw('', 'WORD_USER') == 82, 'Wrong word user raw value returned.');
 
 %% Check for illegal parameter
 
@@ -34,8 +38,21 @@ readback = m.read('', 'AREA_DMAABLE', offset);
 assert(isequal(readback(1:numel(ref)-offset), ref(offset+1:end)), 'Wrong array read back');
 clear readback offset
 
+offset = 5;
+readback = m.read_raw('', 'AREA_DMAABLE', offset);
+disp(readback)
+disp(ref(offset+1:end))
+assert(isequal(readback(1:numel(ref)-offset), ref(offset+1:end)), 'Wrong array read back');
+clear readback offset
+
 elements = 5;
 readback = m.read('', 'AREA_DMAABLE', 0, elements);
+assert(numel(readback) == elements, 'Wrong number of elements read back');
+assert(isequal(readback, ref(1:elements)), 'Wrong array read back');
+clear readback offset
+
+elements = 5;
+readback = m.read_raw('', 'AREA_DMAABLE', 0, elements);
 assert(numel(readback) == elements, 'Wrong number of elements read back');
 assert(isequal(readback, ref(1:elements)), 'Wrong array read back');
 clear readback offset
@@ -47,3 +64,9 @@ assert(numel(readback) == elements, 'Wrong number of elements read back');
 assert(isequal(readback, ref(offset+1:offset+elements)), 'Wrong array read back');
 clear readback offset
 
+offset = 4;
+elements = 10;
+readback = m.read_raw('', 'AREA_DMAABLE', offset, elements);
+assert(numel(readback) == elements, 'Wrong number of elements read back');
+assert(isequal(readback, ref(offset+1:offset+elements)), 'Wrong array read back');
+clear readback offset
